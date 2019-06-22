0bdb38f7e9fb65d08da8b237000f9f341a86b9d9
dohertre <dohertre@rose-hulman.edu>
2019-06-21 23:07:28 -0400

    random

--- src/m1_Point.py
+++ src/m1_Point.py
@@ -117,6 +117,10 @@ def run_test_init():
     #        "pattern matching" from examples.
     #        ASK QUESIONS AS NEEDED.  COMMIT YOUR WORK.
     # ------------------------------------------------------------------
+    print()
+    print('-----------------------------------------------------------')
+    print('Testing the   __init__   method of the Point class.')
+    print('-----------------------------------------------------------')
     p1 = Point(30, 18)
     print()
     print('Expected for p1: 30 18')
@@ -135,10 +139,6 @@ def run_test_init():
     print('Actual for p1:  ', p1.x, p1.y)
     print('Expected for p2: 100 -40')
     print('Actual for p2:  ', p2.x, p2.y)
-    print()
-    print('-----------------------------------------------------------')
-    print('Testing the   __init__   method of the Point class.')
-    print('-----------------------------------------------------------')
 
 
 class Point(object):
@@ -197,6 +197,12 @@ def run_test_repr():
     # DONE: 4.  Follow the same instructions as in TO-DO 3 above,
     #           but for the  __repr__  method specified above.
     # ------------------------------------------------------------------
+
+    print()
+    print('-----------------------------------------------------------')
+    print('Testing the   __repr__   method of the Point class.')
+    print('-----------------------------------------------------------')
+
     p1 = Point(30, 18)
     print()
     print('Expected for p1: Point(30, 18)')
@@ -215,11 +221,6 @@ def run_test_repr():
     print('Actual for p1:  ', p1)
     print('Expected for p2: Point(100, -40)')
     print('Actual for p2:  ', p2)
-
-    print()
-    print('-----------------------------------------------------------')
-    print('Testing the   __repr__   method of the Point class.')
-    print('-----------------------------------------------------------')
 
 
 def run_test_clone():
@@ -277,10 +278,47 @@ def run_test_clone():
     # TODO: 5.  Follow the same instructions as in TO-DO 3 above,
     #           but for the  clone  method specified above.
     # ------------------------------------------------------------------
+
     print()
     print('-----------------------------------------------------------')
     print('Testing the   clone   method of the Point class.')
     print('-----------------------------------------------------------')
+
+    p1 = Point(10, 8)
+    print()
+    print('Expected for p1: Point(10, 8)')
+    print('Actual for p1:  ', p1)
+
+    p2 = p1.clone()
+    p3 = p2.clone()
+    print()
+    print('Expected for p1: Point(10, 8)')
+    print('Actual for p1:  ', p1)
+    print('Expected for p2: Point(10, 8)')
+    print('Actual for p2:  ', p2)
+    print('Expected for p3: Point(10, 8)')
+    print('Actual for p3:  ', p3)
+
+    p1.x = 999
+    print()
+    print('Expected for p1: Point(999, 8)')
+    print('Actual for p1:  ', p1)
+    print('Expected for p2: Point(10, 8)')
+    print('Actual for p2:  ', p2)
+    print('Expected for p3: Point(10, 8)')
+    print('Actual for p3:  ', p3)
+
+    p1.y = 333
+    p2 = Point(11, 22)
+    p3.x = 777
+    p3.y = 555
+    print()
+    print('Expected for p1: Point(999. 333)')
+    print('Actual for p1:  ', p1)
+    print('Expected for p2: Point(11, 22)')
+    print('Actual for p2:  ', p2)
+    print('Expected for p3: Point(777, 555)')
+    print('Actual for p3:  ', p3)
 
 
 def run_test_move_to():
